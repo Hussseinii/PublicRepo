48970a5d9fba1e6239599a835256d804a57c4358
lunga <s929577@stud.hioa.no>
2017-02-07 14:33:41 +0100

    added more methods settinn TOLk, ADMIN;KUNDE

--- TolkesentralenLH/TolkesentralenLH/Models/DbPerson.cs
+++ TolkesentralenLH/TolkesentralenLH/Models/DbPerson.cs
@@ -7,6 +7,141 @@ namespace TolkesentralenLH.Models
 {
     public class DbPerson
     {
+        DbNetcont db = new DbNetcont();
+        /// <summary>
+        /// SettInn en kunde
+        /// </summary>
+        /// <param name="innkunde"></param>
+        /// <returns></returns>
+        public bool settInnKunde(Kunde innkunde)
+        {
+            var nykunde = new Kunde()
+            {
+                persId = innkunde.persId,
+                fornavn = innkunde.fornavn,
+                etternavn = innkunde.etternavn,
+                email = innkunde.email,
+                adresse = innkunde.adresse,
+                regDato = innkunde.regDato,
+                password = innkunde.password,
 
+            };
+            var db = new DbNetcont();
+            try
+            {
+                var eksistererPostnr = db.Poststeder.Find(innkunde.poststed.postNr);
+
+                if (eksistererPostnr == null)
+                {
+                    var nyttpoststed = new Poststed()
+                    {
+                        postNr = innkunde.poststed.postNr,
+                        postSted = innkunde.poststed.postSted
+
+                    };
+                    nykunde.poststed = nyttpoststed;
+
+                }
+                db.Personer.Add(nykunde);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception feil)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// SettInn en Tolk
+        /// </summary>
+        /// <param name="inntolk"></param>
+        /// <returns></returns>
+        public bool settinnTolk(Tolk inntolk)
+        {
+
+            var nyTolk = new Tolk()
+            {
+                persId = inntolk.persId,
+                fornavn = inntolk.fornavn,
+                etternavn = inntolk.etternavn,
+                email = inntolk.email,
+                adresse = inntolk.adresse,
+                regDato = inntolk.regDato,
+                password = inntolk.password,
+
+            };
+            var db = new DbNetcont();
+            try
+            {
+                var eksistererPostnr = db.Poststeder.Find(inntolk.poststed.postNr);
+
+                if (eksistererPostnr == null)
+                {
+                    var nyttpoststed = new Poststed()
+                    {
+                        postNr = inntolk.poststed.postNr,
+                        postSted = inntolk.poststed.postSted
+
+                    };
+                    nyTolk.poststed = nyttpoststed;
+
+                }
+                db.Personer.Add (nyTolk);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception feil)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Setter inn en Amdministrator
+        /// </summary>
+        /// <param name="innAdmin"></param>
+        /// <returns></returns>
+        public bool settinnAdmin(Admin innAdmin)
+        {
+
+            var nyTolk = new Admin()
+            {
+                persId = innAdmin.persId,
+                fornavn = innAdmin.fornavn,
+                etternavn = innAdmin.etternavn,
+                email = innAdmin.email,
+                adresse = innAdmin.adresse,
+                regDato = innAdmin.regDato,
+                password = innAdmin.password,
+
+            };
+            var db = new DbNetcont();
+            try
+            {
+                var eksistererPostnr = db.Poststeder.Find(innAdmin.poststed.postNr);
+
+                if (eksistererPostnr == null)
+                {
+                    var nyttpoststed = new Poststed()
+                    {
+                        postNr = innAdmin.poststed.postNr,
+                        postSted = innAdmin.poststed.postSted
+
+                    };
+                    nyTolk.poststed = nyttpoststed;
+
+                }
+                db.Personer.Add(nyTolk);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception feil)
+            {
+                return false;
+            }
+        }
     }
 }
---
+++ TolkesentralenLH/TolkesentralenLH/Models/FKunde.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TolkesentralenLH.Models
+{
+    public class FKunde
+    {
+        public string fornavn { get; set; }
+        public string etternavn { get; set; }
+
+        public string email { get; set; }
+        public string adresse { get; set; }
+
+        public DateTime regDato { get; set; }
+
+        public string password { get; set; }
+
+    }
+}

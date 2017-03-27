c62dede04d2bd7577aa3b00588e9b1947ef7a96c
lunga <s929577@stud.hioa.no>
2017-03-27 18:18:16 +0200

    Adding husseins repo

---
+++ TolkesentralenLH/TolkesentralenLH/app/_models/models.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NavbarElement = (function () {
+    function NavbarElement() {
+    }
+    return NavbarElement;
+}());
+exports.NavbarElement = NavbarElement;
+var Oppdrag = (function () {
+    function Oppdrag() {
+    }
+    return Oppdrag;
+}());
+exports.Oppdrag = Oppdrag;
+var Tolk = (function () {
+    function Tolk() {
+    }
+    return Tolk;
+}());
+exports.Tolk = Tolk;
+var Oversettelse = (function () {
+    function Oversettelse() {
+    }
+    return Oversettelse;
+}());
+exports.Oversettelse = Oversettelse;
+var Kunde = (function () {
+    function Kunde() {
+    }
+    return Kunde;
+}());
+exports.Kunde = Kunde;
+//# sourceMappingURL=models.js.map
